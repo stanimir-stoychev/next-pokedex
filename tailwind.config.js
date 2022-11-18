a255d0bc41552afd4780db85e51e06ef3f83bfcd
Stanimir Stoychev <xstukix@gmail.com>
2022-11-18 11:42:20 +0100

    Extend tailwind with colors - css variables

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,26 @@
 /** @type {import('tailwindcss').Config} */
+
 module.exports = {
     content: ['./pages/**/*.{js,ts,jsx,tsx}', './src/**/*.{js,ts,jsx,tsx}'],
     theme: {
-        extend: {},
+        extend: {
+            colors: {
+                primary: {
+                    dark: 'rgb(var(--color-primary-dark) / <alpha-value>)',
+                    DEFAULT: 'rgb(var(--color-primary-main) / <alpha-value>)',
+                    light: 'rgb(var(--color-primary-light) / <alpha-value>)',
+                },
+                secondary: {
+                    dark: 'rgb(var(--color-secondary-dark) / <alpha-value>)',
+                    DEFAULT: 'rgb(var(--color-secondary-main) / <alpha-value>)',
+                    light: 'rgb(var(--color-secondary-light) / <alpha-value>)',
+                },
+                info: 'rgb(var(--color-info) / <alpha-value>)',
+                success: 'rgb(var(--color-success) / <alpha-value>)',
+                error: 'rgb(var(--color-error) / <alpha-value>)',
+                warning: 'rgb(var(--color-warning) / <alpha-value>)',
+            },
+        },
     },
     plugins: [],
 };
