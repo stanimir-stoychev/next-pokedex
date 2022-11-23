eb9776f103d6ac6c08b8c2b0c5ae88b8be401d88
Stanimir Stoychev <xstukix@gmail.com>
2022-11-23 16:22:22 +0100

    Add single pokemon page

--- pages/pokemon/[name].tsx
+++ pages/pokemon/[name].tsx
@@ -1,7 +1,11 @@
 import clsx from 'clsx';
+import NextImage from 'next/image';
 
 import { usePokemonQuery } from '@/src/queries/pokedex';
 import { useRouter } from 'next/router';
+import { PokemonStats, PokemonTypes } from '@/src/components';
+
+// https://commons.wikimedia.org/wiki/Category:Pok%C3%A9mon_types_icons
 
 export default function PokemonPage() {
     const {
@@ -9,12 +13,25 @@ export default function PokemonPage() {
     } = useRouter();
 
     const pName = (Array.isArray(name) ? name[0] : name) ?? 'bulbasaur';
-    const pokemonQuery = usePokemonQuery({ name: pName, url: '' });
+    const pokemon = usePokemonQuery({ name: pName, url: '' });
+    const artworkSrc = pokemon.data?.sprites.other['official-artwork'].front_default;
 
     return (
-        <>
-            WIP...
-            {/* <pre>{JSON.stringify(pokemonQuery.data, null, 4)}</pre> */}
-        </>
+        <div className={clsx('container', 'm-auto', 'p-4', 'space-y-4')}>
+            {artworkSrc && (
+                <div className={clsx('relative', 'w-full', 'h-64')}>
+                    <NextImage
+                        src={artworkSrc}
+                        alt={pName}
+                        fill
+                        style={{ objectFit: 'contain' }}
+                        className={clsx('hover:scale-150', 'transition', 'ease-in-out')}
+                    />
+                </div>
+            )}
+            <h1 className={clsx('capitalize', 'text-3xl')}>{pName}</h1>
+            <PokemonStats addLabels stats={pokemon.data?.stats ?? []} />
+            <PokemonTypes addLabels types={pokemon.data?.types ?? []} />
+        </div>
     );
 }
